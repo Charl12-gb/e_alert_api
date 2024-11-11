2791b894c505fbff587fc19a162f43f4a439e69f
Charles GBOYOU <gboyoucharles22@gmail.com>
2024-11-11 20:02:19 +0100

    Cron deux fois par jours

--- e_alert/settings.py
+++ e_alert/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 ]
 
 CRONJOBS = [
-    ('*/5 * * * *', 'my_api.Utils.cron.start_cron'),  # Toutes les 5 minutes
+    ('0 8,16 * * *', 'my_api.Utils.cron.start_cron'),  # Toutes les 5 minutes
 ]
 
 logger = logging.getLogger('django_auth_ldap')
